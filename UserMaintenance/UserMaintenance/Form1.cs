0ec21418b783e167d5a9ae1f7f0196466ffddf7d
WebWarazslok <gergely.boglari@stud.uni-corvinus.hu>
2024-03-26 11:58:13 +0100

    Teljes névvé alakítás csv miatt.

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -9,8 +9,7 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName; // label1
-            label2.Text = Resource1.FirstName; // label2
+            label1.Text = Resource1.FullName; // label1
             button1.Text = Resource1.Add; // button1
 
             listBox1.DataSource = users;
@@ -27,8 +26,7 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = label1.Text,
-                FirstName = label2.Text
+                FullName = label1.Text+ " " + label2.Text,
             };
             users.Add(u);
         }
