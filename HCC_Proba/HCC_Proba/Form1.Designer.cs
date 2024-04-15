cb6d25ba554d48b4b41dad4daf77f13d514cfdc5
WebWarazslok <gergely.boglari@stud.uni-corvinus.hu>
2024-04-15 21:39:08 +0200

    Base elements included, POST request, GiftCardCreation successful

--- HCC_Proba/HCC_Proba/Form1.Designer.cs
+++ HCC_Proba/HCC_Proba/Form1.Designer.cs
@@ -29,30 +29,109 @@
         private void InitializeComponent()
         {
             richTextBox1 = new RichTextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(23, 12);
+            richTextBox1.Location = new Point(12, 12);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(753, 426);
-            richTextBox1.TabIndex = 0;
+            richTextBox1.Size = new Size(728, 411);
+            richTextBox1.TabIndex = 1;
             richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(771, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(251, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(771, 124);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(251, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(771, 211);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(251, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(839, 262);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 61);
+            button1.TabIndex = 6;
+            button1.Text = "Kártya hozzáadása";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += Button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(825, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Kártyatulajdonos neve:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(839, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Vevő e-mail címe:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(825, 188);
+            label3.Name = "label3";
+            label3.Size = new Size(159, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Felhasználható összeg:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1062, 447);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(richTextBox1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Ajándékkártya vásárlás";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
         private RichTextBox richTextBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- HCC_Proba/HCC_Proba/Form1.cs
+++ HCC_Proba/HCC_Proba/Form1.cs
@@ -8,6 +8,10 @@ using System.Windows.Forms;
 using Hotcakes.CommerceDTO.v1;
 using Hotcakes.CommerceDTO.v1.Catalog;
 using Hotcakes.CommerceDTO.v1.Client;
+using Newtonsoft.Json;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
 
 namespace HCC_Proba
 {
@@ -16,70 +20,54 @@ namespace HCC_Proba
 
         private static readonly HttpClient client = new HttpClient();
         private const string ApiKey = "1-2d78ad9c-d5ca-46e7-8cbc-066d8e72b40c";
+        private const string apiUrl = "http://20.234.113.211:8106";
+        private const string apiPath = "/DesktopModules/Hotcakes/API/rest/v1/";
+        private const string endPoint = "giftcards";
+        public int GiftCNum = 1;
+        public List<int> giftNumsList = new List<int>();
+        public Random r = new Random();
 
         public Form1()
         {
             InitializeComponent();
         }
 
+
+        
+
         private async void Form1_Load(object sender, EventArgs e)
         {
-            
-            string apiUrl = "http://20.234.113.211:8106";
-            string apiPath = "/DesktopModules/Hotcakes/API/rest/v1/";
-            string endPoint = "orders";
-            string transactionId = "B12F0CAC-F60A-44F1-BA45-6240201180BC";
-            // Példa GET kérés az API-hoz
-            string response = await GetInfoEndPoint(apiUrl, ApiKey, apiPath,endPoint, transactionId);
 
-           
+            //Regions
+
+            #region Payment status values
+            /*
+             * PaymentStatus == 0 --> Order has not been completed.
+             * PaymentStatus == 1 --> Unpaid.
+             * PaymentStatus == 2 --> Partially paid.
+             * PaymentStatus == 3 --> Paid.
+             * PaymentStatus == 4 --> Overpaid.
+             * */
+            #endregion
             
+            // GET kérés az API-hoz
+            string response = await GetInfoEndPoint(apiUrl, ApiKey, apiPath, endPoint);
+
             // JSON válasz feldolgozása és az eredmény kinyerése
             JObject jsonResponse = JObject.Parse(response);
-
-            // A Content tömb lekérdezése
-            JArray contentArray = (JArray)jsonResponse["Content"];
-            int i = 0;
-            // Az egyes elemek lekérdezése a tömbből
-            foreach (JObject item in contentArray)
-            {
-                
-                if (i< 10&& item["UserEmail"].ToString()!= "info@hotcakescommerce.com")
-                {
-                    string orderbvin = item["bvin"].ToString();
-                    string orderEmail = item["UserEmail"].ToString();
-                    richTextBox1.AppendText($"Order bvin: {orderbvin}; Order Email: {orderEmail}\n");
-                    i++;
-                }
-                
-            }
-
-            //JSON respone feldolgozása tömbbe
-            //string[] result = jsonResponse["Content"].ToString().Split(',');
-
-            // if (jsonResponse["Content"]["bvin"]!=null)
-            //{
-            //string[] result = jsonResponse["Content"]["bvin"].ToString().Split(',');
-              //  richTextBox1.Text += result;
-            //}
-            
-            
-            // Az eredmény megjelenítése a RichTextBox-ban
-
-            /*for (int i = 0; i < result.Length; i++)
-            {
-                richTextBox1.Text += result[i];
-            }*/
+            string result = jsonResponse["Content"].ToString();
+            richTextBox1.Text = result;
 
         }
 
-        static async Task<string> GetInfoEndPoint(string apiUrl, string apiKey, string apiPath, string endPoint, string transID)
+        //GET REQUEST
+        static async Task<string> GetInfoEndPoint(string apiUrl, string apiKey, string apiPath, string endPoint)
         {
-             string catsEndpoint = $"{apiUrl}{apiPath}{endPoint}?key={apiKey}";
+            string Endpoint = $"{apiUrl}{apiPath}{endPoint}?key={apiKey}";
             //string catsEndpoint = $"{apiUrl}{apiPath}{endPoint}/{transID}?key={apiKey}";
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Api-Key", apiKey);
 
-            var response = await client.GetAsync(catsEndpoint);
+            var response = await client.GetAsync(Endpoint);
 
             if (response.IsSuccessStatusCode)
             {
@@ -91,5 +79,69 @@ namespace HCC_Proba
             }
         }
 
+        //POST REQUEST
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            string giftCardNum="";
+            int giftR = r.Next() * 1000;
+
+            // specify the gift card expiration date
+            var expirationDate = DateTime.Now.Date.AddYears(1);
+            if (!giftNumsList.Contains(GiftCNum))
+            {
+                 giftCardNum = $"BOLT-{giftR}-{GiftCNum}";
+                 giftNumsList.Add(GiftCNum);
+                 GiftCNum++;
+            }
+            else
+            {
+                GiftCNum++;
+                giftCardNum = $"BOLT-{giftR}-{GiftCNum}";
+                giftNumsList.Add(GiftCNum);
+            }
+            
+
+            // create a new gift card object with minimal information
+            var giftCard = new
+            {
+                CardNumber = giftCardNum,
+                Amount = Convert.ToInt32(textBox3.Text),
+                ExpirationDateUtc = expirationDate.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
+                RecipientName = textBox1.Text,
+                RecipientEmail = textBox2.Text,
+                Enabled = true
+            };
+
+            // serialize the gift card object to JSON
+            var json = JsonConvert.SerializeObject(giftCard);
+
+            // create a StringContent object with the JSON data
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // build the request URL
+            var requestUrl = $"{apiUrl}{apiPath}{endPoint}?key={ApiKey}";
+
+            try
+            {
+                // send the POST request
+                var response = await client.PostAsync(requestUrl, content);
+
+                // check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show("POST request successful!");
+                }
+                else
+                {
+                    MessageBox.Show("POST request failed. Status code: " + response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error sending POST request: " + ex.Message);
+            }
+            //Form1_Load(sender, e);
+        }
     }
 }
